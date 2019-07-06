86a087d0b593cb2ad203e15b950124dda36fc19c
Barry Buck <jozi011@gmail.com>
2019-07-07 00:54:16 +0200

    working version of logo

--- web/src/features/landing/ui.js
+++ web/src/features/landing/ui.js
@@ -13,27 +13,28 @@ export const LandingPage = ({ ui: { VStack, HStack, Text, Icon } }) =>
         y="center"
         box={{ color: brand.secondary, height: 'full' }}
       >
-        <HStack x="center" y="center">
-          <Icon
-            as="div"
-            name="flash-outline"
-            size="6xl"
-            box={{
-              margin: { right: 4 },
-              display: 'flex',
-              flexDirection: 'col',
-            }}
-            style={{ transform: 'scaleX(-1) rotate(55deg)' }}
-          />
-          <Text
-            size={['4xl', { md: '5xl' }]}
-            family={brand.fontFamily}
-            space="no-wrap"
-            box={{ padding: { bottom: 2 } }}
-          >
-            Z1 App Starter
-          </Text>
-        </HStack>
+        <svg viewBox="0 0 600 500" width="200px" height="200px">
+          <g>
+            <path
+              d=" M 120 470.859 L 598.992 110.86 L 0 22 L 598.992 22 L 0 470.859 L 120 470.859"
+              fill="rgb(0,255,0)"
+            />
+            <path
+              d=" M 480 22 L 1.008 381.998 L 600 470.859 L 1.008 470.859 L 600 22 L 480 22"
+              fill="rgb(19,204,19)"
+            />
+            <path d=" M 588 472 L 2 472 L 128 370" fill="rgb(19,204,19)" />
+            <path d=" M 8 22 L 594 22 L 468 124" fill="rgb(0,255,0)" />
+          </g>
+        </svg>
+        <Text
+          size={['4xl', { md: '5xl' }]}
+          family={brand.fontFamily}
+          space="no-wrap"
+          box={{ padding: { bottom: 2 } }}
+        >
+          Z1 App Starter
+        </Text>
         <Text size="6xl" family={brand.fontFamily} x="center">
           Welcome
         </Text>
--- web/src/features/layout/ui/elements/NavLogo.jsx
+++ web/src/features/layout/ui/elements/NavLogo.jsx
@@ -18,11 +18,21 @@ export const NavLogo = ({ ui: { HStack, Icon } }) => ({
       }}
       exact={true}
     >
-      <Icon
-        name="flash-outline"
-        size="5xl"
-        style={{ transform: 'scaleX(-1) rotate(55deg)' }}
-      />
+      >
+      <svg viewBox="0 0 600 500" width="50px" height="50px">
+          <g>
+            <path
+              d=" M 120 470.859 L 598.992 110.86 L 0 22 L 598.992 22 L 0 470.859 L 120 470.859"
+              fill="rgb(0,255,0)"
+            />
+            <path
+              d=" M 480 22 L 1.008 381.998 L 600 470.859 L 1.008 470.859 L 600 22 L 480 22"
+              fill="rgb(19,204,19)"
+            />
+            <path d=" M 588 472 L 2 472 L 128 370" fill="rgb(19,204,19)" />
+            <path d=" M 8 22 L 594 22 L 468 124" fill="rgb(0,255,0)" />
+          </g>
+        </svg>
     </HStack>
   )
 }
