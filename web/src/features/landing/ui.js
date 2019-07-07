b0bf6d7ce14ce13cad77f032d85a26f38ec523c9
Barry Buck <jozi011@gmail.com>
2019-07-07 02:24:39 +0200

    working version of logo

--- web/src/features/landing/ui.js
+++ web/src/features/landing/ui.js
@@ -13,27 +13,26 @@ export const LandingPage = ({ ui: { VStack, HStack, Text, Icon } }) =>
         y="center"
         box={{ color: brand.secondary, height: 'full' }}
       >
-        <svg viewBox="0 0 627 500" width="200px" height="200px">
+         <svg viewBox="0 0 627 500" width="200px" height="200px">
+        <g>
           <g>
+            <path
+              d=" M 125 475.859 L 603.992 115.86 L 14 23 L 601 23 L 5 475.859 L 125 475.859"
+              fill="rgb(56,161,105)"
+            />
             <g>
               <path
-                d=" M 125 475.859 L 603.992 115.86 L 14 23 L 601 23 L 5 475.859 L 125 475.859"
-                fill="rgb(80,161,77)"
+                d=" M 486.202 22 L 365.431 113.492 L 11 381.998 L 602 473 L 12.008 470.859 L 605 22 L 486.202 22"
+                fill="rgb(72,187,120)"
               />
-
-              <g>
-                <path
-                  d=" M 486.202 22 L 365.431 113.492 L 11 381.998 L 602 473 L 12.008 470.859 L 605 22 L 486.202 22"
-                  fill="rgb(113,182,89)"
-                />
-              </g>
-            </g>
-            <g>
-              <path d=" M 602 473 L 2 476 L 137 377" fill="rgb(80,161,77)" />
-              <path d=" M 9 22 L 604 22 L 469 124" fill="rgb(113,182,89)" />
             </g>
           </g>
-        </svg>
+          <g>
+            <path d=" M 602 473 L 2 476 L 137 377" fill="rgb(56,161,105)" />
+            <path d=" M 9 22 L 604 22 L 469 124" fill="rgb(72,187,120)" />
+          </g>
+        </g>
+      </svg>
         <Text
           size={['4xl', { md: '5xl' }]}
           family={brand.fontFamily}
--- web/src/features/layout/ui/elements/NavLogo.jsx
+++ web/src/features/layout/ui/elements/NavLogo.jsx
@@ -18,24 +18,23 @@ export const NavLogo = ({ ui: { HStack, Icon } }) => ({
       }}
       exact={true}
     >
-      <svg viewBox="0 0 627 500" width="50px" height="50px">
+      <svg viewBox="0 0 627 500" width="60px" height="60px">
         <g>
           <g>
             <path
               d=" M 125 475.859 L 603.992 115.86 L 14 23 L 601 23 L 5 475.859 L 125 475.859"
-              fill="rgb(80,161,77)"
+              fill="rgb(56,161,105)"
             />
-
             <g>
               <path
                 d=" M 486.202 22 L 365.431 113.492 L 11 381.998 L 602 473 L 12.008 470.859 L 605 22 L 486.202 22"
-                fill="rgb(113,182,89)"
+                fill="rgb(72,187,120)"
               />
             </g>
           </g>
           <g>
-            <path d=" M 602 473 L 2 476 L 137 377" fill="rgb(80,161,77)" />
-            <path d=" M 9 22 L 604 22 L 469 124" fill="rgb(113,182,89)" />
+            <path d=" M 602 473 L 2 476 L 137 377" fill="rgb(56,161,105)" />
+            <path d=" M 9 22 L 604 22 L 469 124" fill="rgb(72,187,120)" />
           </g>
         </g>
       </svg>
